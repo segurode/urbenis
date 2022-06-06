52824dfaf900b5ca9d4841945fd07daec563ae1e
Albert <avilardell@outlook.com>
2022-06-06 19:03:22 +0200

    helmet

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Helmet>
-        <title>React helmet powered</title>
+        <title>React helmet</title>
       </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
