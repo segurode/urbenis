43b172ed1121732c17df0dd298c90b487812dd87
Albert <avilardell@outlook.com>
2022-06-06 18:57:17 +0200

    helmet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {Helmet} from 'react-helmet'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>React helmet powered</title>
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
